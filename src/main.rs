6d60471aa4d8143c867e6820d5ed50ca51d6461b
sunray760nm <924393422@qq.com>
2024-11-07 19:48:42 +0800

    test git user name

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    println!("Hello, world!");
     let listener = TcpListener::bind("127.0.0.1:0")
         .expect("Failed to bind random port");
 
